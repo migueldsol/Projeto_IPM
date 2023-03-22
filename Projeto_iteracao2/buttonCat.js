12d1ab9fe47fe5b9ca5d84ed09fd4286c49acc4f
BernardoSousa03 <bernardo.de.sousa@tecnico.ulisboa.pt>
2023-03-22 01:00:43 +0000

    Iteração 2

---
+++ Projeto_iteracao2/buttonCat.js
@@ -0,0 +1,37 @@
+// ButtonCAT class (position and width)
+class ButtonCat
+{
+  constructor(x, y, w, l, ownTargets){
+    this.x      = x;
+    this.y      = y;
+    this.width  = w;
+    this.label  = l;
+    this.ownTargets = ownTargets;
+  }
+  
+  // Checks if a mouse click took place
+  // within the target
+  clicked(mouse_x, mouse_y)
+  {
+    return dist(this.x, this.y, mouse_x, mouse_y) < this.width / 2;
+  }
+  
+  // Draws the target (i.e., a circle)
+  // and its label
+  draw()
+  {
+    // Draw target
+    fill(color(155,155,155));                 
+    circle(this.x, this.y, this.width);
+
+    // Draw label
+    textFont("Arial", 12);
+    fill(color(255,255,255));
+    textAlign(CENTER);
+    text(this.label, this.x, this.y);
+  }
+  
+  targets(){
+    return this.ownTargets
+  }
+}
