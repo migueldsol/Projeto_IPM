2e09ad3d481a5d9b6c2edea7da4f445bbb05bba9
BernardoSousa03 <bernardo.de.sousa@tecnico.ulisboa.pt>
2023-03-27 14:26:17 +0100

    added prototype 3

---
+++ Projeto_iteracao2 copy/target.js
@@ -0,0 +1,62 @@
+// Target class (position and width)
+class Target {
+  constructor(x, y, w, l, id, color) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.label = l;
+    this.id = id;
+    this.drawable = false;
+    this.color = color;
+  }
+
+  // Checks if a mouse click took place
+  // within the target
+  clicked(mouse_x, mouse_y) {
+    return dist(this.x, this.y, mouse_x, mouse_y) < this.width / 2;
+  }
+
+  // Draws the target (i.e., a circle)
+  // and its label
+  draw() {
+    // Draw target
+    fill(color(this.color[0], this.color[1], this.color[2]));
+    circle(this.x, this.y, this.width);
+
+    // Draw label
+    textFont("Arial", 12);
+    fill(color(255, 255, 255));
+    textAlign(CENTER);
+    text(this.label, this.x, this.y);
+  }
+
+  //Returns if we should draw the target?
+  isDrawn() {
+    return this.drawable;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  alterTarget(x, y) {
+    this.x = x;
+    this.y = y;
+    this.drawable = true;
+  }
+  makeDrawable() {
+    this.drawable = true;
+  }
+
+  makeNotDrawable() {
+    this.drawable = false;
+  }
+
+  changeColor(color){
+    this.color = color;
+  }
+
+  getLabel(){
+    return this.label;
+  }
+}
